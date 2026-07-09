001be48f4e2b59e3f701fb2545fa1744fc7ddcd9
nammilo1105-lang <nammilo1105@gmail.com>
2026-07-09 13:45:52 +0700

    Fix MongoDB config

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongo: {
-        uri: process.env.URI || "MONGO_URI=mongodb+srv://namhoangthe123_db_user:nam2012@phone-shop.gexgyma.mongodb.net/phone_shop_api",
+        uri: process.env.URI || "mongodb+srv://namhoangthe123_db_user:nam2012@phone-shop.gexgyma.mongodb.net/phone_shop_api?appName=phone-shop",
     },
 }
--- src/bin/www.js
+++ src/bin/www.js
@@ -1,7 +1,8 @@
 const app = require("../apps/app")
 const config = require("config")
 // const open = require("open")
-const server =  app.listen(port = config.get("app.serverPort"), async () => {
+const port = process.env.PORT || config.get("app.serverPort");
+const server =  app.listen(port, () => {
     console.log(`server running on port ${port}`);
     console.log(`Swagger: http://localhost:${port}/api-docs`);
     // open(`http://localhost:${port}/api-docs`);
